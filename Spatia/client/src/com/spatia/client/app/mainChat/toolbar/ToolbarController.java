6e01e41c1d34904c6ee9d689eb7a5aa2a1d7b766
TiagoSD22 <tiagosdionizio@gmail.com>
2020-08-25 19:33:48 -0300

    adicionando botao para atualizar lista de salas existentes, adicionando card para criacao de nova sala

--- Spatia/client/src/com/spatia/client/app/mainChat/toolbar/ToolbarController.java
+++ Spatia/client/src/com/spatia/client/app/mainChat/toolbar/ToolbarController.java
@@ -1,9 +1,12 @@
 package com.spatia.client.app.mainChat.toolbar;
 
+import com.jfoenix.controls.JFXTabPane;
+import com.jfoenix.controls.JFXTextField;
 import com.spatia.client.app.mainChat.MainChatController;
 import com.spatia.client.app.services.AudioService;
 import com.jfoenix.controls.JFXButton;
 import com.spatia.common.Client;
+import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -18,6 +21,7 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 import org.apache.commons.collections4.BidiMap;
 import org.apache.commons.collections4.bidimap.DualHashBidiMap;
 
@@ -40,11 +44,25 @@ public class ToolbarController {
     @FXML
     private JFXButton soundBt;
     @FXML
+    private JFXTabPane chatTabPane;
+    @FXML
     private Tab chatTab;
     @FXML
     private Tab roomsTab;
     @FXML
     private ListView<GridPane> roomListView;
+    @FXML
+    private JFXButton createRoomBt;
+    @FXML
+    private JFXButton refreshRoomListBt;
+    @FXML
+    private GridPane createRoomCard;
+    @FXML
+    private JFXTextField createRoomNameTf;
+    @FXML
+    private JFXButton createRoomConfirmBt;
+    @FXML
+    private JFXButton createRoomCancelBt;
 
     private MainChatController mainChatController;
     private ContactInfoBox currentSelectedContactInfoBox;
@@ -54,6 +72,8 @@ public class ToolbarController {
     private boolean sound = true;
     private ImageView soundOnImage;
     private ImageView soundOffImage;
+    private TranslateTransition showCreateRoomCard;
+    private TranslateTransition hideCreateRoomCard;
 
     public void init(MainChatController mainChatController) {
         this.mainChatController = mainChatController;
@@ -87,9 +107,39 @@ public class ToolbarController {
                 new Image(getClass().getResourceAsStream("/assets/Images/rooms_icon.png"))
         ));
 
+        chatTabPane.getSelectionModel().selectedItemProperty().addListener((ov, oldTab, newTab) -> {
+            if(newTab.equals(roomsTab)){
+                onRoomTabClicked();
+            }
+            else{
+                onChaTabClicked();
+            }
+        });
+
         clientContactInfoBoxMap = new DualHashBidiMap<>();
         clientUnreadMsgRegisterMap = new HashMap<>();
+
+        loadShowCreateRoomCardAnimation();
+        loadHideCreateRoomCardAnimation();
         //contactInfoBoxClientMap = new HashMap<>();
+    }
+
+    private void loadShowCreateRoomCardAnimation(){
+        showCreateRoomCard =new TranslateTransition(new Duration(250), createRoomCard);
+        showCreateRoomCard.setFromX(-400);
+        showCreateRoomCard.setToX(0);
+    }
+
+    private void loadHideCreateRoomCardAnimation(){
+        hideCreateRoomCard = new TranslateTransition(new Duration(250), createRoomCard);
+        hideCreateRoomCard.setFromX(0);
+        hideCreateRoomCard.setToX(-400);
+
+        hideCreateRoomCard.setOnFinished(event -> {
+            createRoomCard.setVisible(false);
+            roomListView.setVisible(true);
+            createRoomNameTf.clear();
+        });
     }
 
     private void setCurrentUserName(String name) {
@@ -191,5 +241,30 @@ public class ToolbarController {
 
     public void onCreateRoomBtClicked(){
         System.out.println("Solicitando criar nova sala");
+
+        roomListView.setVisible(false);
+        createRoomCard.setVisible(true);
+
+        showCreateRoomCard.play();
+    }
+
+    private void onChaTabClicked(){
+        System.out.println("Clicou na tab de chat");
+    }
+
+    private void onRoomTabClicked(){
+        System.out.println("Clicou na tab de salas");
+    }
+
+    public void onRefreshRoomListBtClicked(){
+
+    }
+
+    public void onCreateRoomConfirmBtClicked(){
+        hideCreateRoomCard.play();
+    }
+
+    public void onCreateRoomCancelBtClicked(){
+        hideCreateRoomCard.play();
     }
 }
