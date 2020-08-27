96fbb610b7a70c615cee537c91fc3ef8a8d8718b
TiagoSD22 <tiagosdionizio@gmail.com>
2020-08-26 23:20:40 -0300

    adicionando modelos de solicitacao de interacao com sala

--- Spatia/common/src/main/java/com/spatia/common/ChatRoomInteraction.java
+++ Spatia/common/src/main/java/com/spatia/common/ChatRoomInteraction.java
@@ -1,4 +1,45 @@
 package com.spatia.common;
 
+import com.gigaspaces.annotation.pojo.SpaceClass;
+import com.gigaspaces.annotation.pojo.SpaceId;
+
+@SpaceClass
 public class ChatRoomInteraction {
+    private InteractionType type;
+    private String roomName;
+    private Client client;
+
+    public ChatRoomInteraction() {
+    }
+
+    public ChatRoomInteraction(InteractionType type, String roomName, Client client) {
+        this.type = type;
+        this.roomName = roomName;
+        this.client = client;
+    }
+
+    public InteractionType getType() {
+        return type;
+    }
+
+    public void setType(InteractionType type) {
+        this.type = type;
+    }
+
+    @SpaceId(autoGenerate = false)
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 }
--- Spatia/common/src/main/java/com/spatia/common/InteractionType.java
+++ Spatia/common/src/main/java/com/spatia/common/InteractionType.java
@@ -1,4 +1,22 @@
 package com.spatia.common;
 
-public enum InteractionType {
+import java.io.Serializable;
+
+public enum InteractionType implements Serializable {
+    ENTER("ENTER"),
+    LEAVE("LEAVE");
+
+    private String value;
+
+    InteractionType(String value){
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value){
+        this.value = value;
+    }
 }
