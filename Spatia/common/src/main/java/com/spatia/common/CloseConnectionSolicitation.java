9980bc04d3898a35bfaed190064a0a7806ed8a2d
TiagoSD22 <tiagosdionizio@gmail.com>
2020-08-27 01:19:10 -0300

    adicionando modelo para solicitacao de desconexao

--- Spatia/common/src/main/java/com/spatia/common/CloseConnectionSolicitation.java
+++ Spatia/common/src/main/java/com/spatia/common/CloseConnectionSolicitation.java
@@ -1,4 +1,35 @@
 package com.spatia.common;
 
+import com.gigaspaces.annotation.pojo.SpaceClass;
+import com.gigaspaces.annotation.pojo.SpaceId;
+
+@SpaceClass
 public class CloseConnectionSolicitation {
+    private String userName;
+    private String currentInRoomName;
+
+    public CloseConnectionSolicitation() {
+    }
+
+    public CloseConnectionSolicitation(String userName, String currentInRoomName) {
+        this.userName = userName;
+        this.currentInRoomName = currentInRoomName;
+    }
+
+    @SpaceId(autoGenerate = false)
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getCurrentInRoomName() {
+        return currentInRoomName;
+    }
+
+    public void setCurrentInRoomName(String currentInRoomName) {
+        this.currentInRoomName = currentInRoomName;
+    }
 }
