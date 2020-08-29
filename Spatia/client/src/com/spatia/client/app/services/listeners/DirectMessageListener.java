a1f095f014499389d8c5eadb9d97caba5ee088f9
TiagoSD22 <tiagosdionizio@gmail.com>
2020-08-29 00:37:11 -0300

    adicionando listeners para observar novas tuplas de mensgaens diretas e mensagens para a sala no espaco

--- Spatia/client/src/com/spatia/client/app/services/listeners/DirectMessageListener.java
+++ Spatia/client/src/com/spatia/client/app/services/listeners/DirectMessageListener.java
@@ -1,4 +1,22 @@
 package com.spatia.client.app.services.listeners;
 
+import com.spatia.client.app.services.SpaceHandler;
+import com.spatia.common.ChatMessage;
+import org.openspaces.events.EventDriven;
+import org.openspaces.events.TransactionalEvent;
+import org.openspaces.events.adapter.SpaceDataEvent;
+import org.openspaces.events.notify.NotifyType;
+import org.openspaces.events.polling.Polling;
+
+@EventDriven
+@Polling
+@NotifyType(write = true)
+@TransactionalEvent
 public class DirectMessageListener {
+    public DirectMessageListener() {}
+
+    @SpaceDataEvent
+    public void eventListener(ChatMessage event){
+        SpaceHandler.getInstance().onDirectMessageReceived(event);
+    }
 }
--- Spatia/client/src/com/spatia/client/app/services/listeners/RoomMessageListener.java
+++ Spatia/client/src/com/spatia/client/app/services/listeners/RoomMessageListener.java
@@ -1,4 +1,24 @@
 package com.spatia.client.app.services.listeners;
 
+import com.spatia.client.app.services.SpaceHandler;
+import com.spatia.common.ChatRoomMessage;
+import org.openspaces.events.EventDriven;
+import org.openspaces.events.TransactionalEvent;
+import org.openspaces.events.adapter.SpaceDataEvent;
+import org.openspaces.events.notify.Notify;
+import org.openspaces.events.notify.NotifyType;
+
+@EventDriven
+@Notify
+@NotifyType(write = true)
+@TransactionalEvent
 public class RoomMessageListener {
+
+    public RoomMessageListener() {}
+
+    @SpaceDataEvent
+    public void eventListener(ChatRoomMessage event){
+        SpaceHandler spaceHandlerInstance = SpaceHandler.getInstance();
+        spaceHandlerInstance.onRoomMessageReceived(event);
+    }
 }
