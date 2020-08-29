ba30dbb1807721cf30e00565dece29d903de2e3e
TiagoSD22 <tiagosdionizio@gmail.com>
2020-08-29 00:36:33 -0300

    ajustando modelo de mensagem para sala

--- Spatia/common/src/main/java/com/spatia/common/ChatRoomMessage.java
+++ Spatia/common/src/main/java/com/spatia/common/ChatRoomMessage.java
@@ -1,23 +1,44 @@
 package com.spatia.common;
 
 import com.gigaspaces.annotation.pojo.SpaceClass;
+import com.gigaspaces.annotation.pojo.SpaceId;
 
 @SpaceClass
-public class ChatRoomMessage extends ChatMessage {
+public class ChatRoomMessage {
     private String roomName;
+    private String senderName;
+    private String text;
 
-    public ChatRoomMessage(){super();}
+    public ChatRoomMessage(){}
 
-    public ChatRoomMessage(String roomName, String senderName, String receiverName, String text) {
-        super(senderName, receiverName, text);
+    public ChatRoomMessage(String roomName, String senderName, String text) {
         this.roomName = roomName;
+        this.senderName = senderName;
+        this.text = text;
     }
 
+    @SpaceId(autoGenerate = false)
     public String getRoomName() {
         return roomName;
     }
 
     public void setRoomName(String roomName) {
         this.roomName = roomName;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 }
