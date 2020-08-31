f4d99d020a82de09ecf292ef3868657a1d6e556e
TiagoSD22 <tiagosdionizio@gmail.com>
2020-08-31 01:41:04 -0300

    adicionando listener para notificacao de alteracao de status de digitacao

--- Spatia/client/src/com/spatia/client/app/services/listeners/TypingStatusListener.java
+++ Spatia/client/src/com/spatia/client/app/services/listeners/TypingStatusListener.java
@@ -1,4 +1,24 @@
 package com.spatia.client.app.services.listeners;
 
+import com.spatia.client.app.services.SpaceHandler;
+import com.spatia.common.UpdateTypingStatus;
+import org.openspaces.events.EventDriven;
+import org.openspaces.events.TransactionalEvent;
+import org.openspaces.events.adapter.SpaceDataEvent;
+import org.openspaces.events.notify.NotifyType;
+import org.openspaces.events.polling.Polling;
+
+@EventDriven
+@Polling
+@NotifyType(write = true)
+@TransactionalEvent
 public class TypingStatusListener {
+    public TypingStatusListener() {
+    }
+
+    @SpaceDataEvent
+    public void eventListener(UpdateTypingStatus event){
+        SpaceHandler spaceHandlerInstance = SpaceHandler.getInstance();
+        spaceHandlerInstance.onUpdateTypingStatusReceived(event);
+    }
 }
